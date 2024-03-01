cadfa74bf90d1ffc5a066bb388c1831ee5ec3cb6
fernandojec <fernando.riyo@jec.co.id>
2024-03-01 14:36:50 +0700

    Add StructToString

--- pkg/stringTools/stringTools.go
+++ pkg/stringTools/stringTools.go
@@ -1,6 +1,10 @@
 package stringtools
 
-import "strings"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 func RightValue(input string, length int) string {
 	if len(input) <= length {
@@ -35,4 +39,37 @@ func ParseStringToBoolPtr(s string) *bool {
 		return nil
 	}
 	return &result
+}
+
+func StructToString(s interface{}) string {
+	v := reflect.ValueOf(s)
+	t := v.Type()
+
+	var sb strings.Builder
+
+	for i := 0; i < v.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("json")
+		tagName := strings.Split(tag, ",")[0] // Get the JSON field name, ignore options like omitempty
+
+		// Skip if the field has no json tag or is omitted
+		if tagName == "" || tagName == "-" {
+			continue
+		}
+
+		value := v.Field(i).Interface()
+
+		// Special handling for *bool to avoid dereferencing nil pointers
+		if v.Field(i).Kind() == reflect.Ptr && v.Field(i).IsNil() {
+			continue // Skip nil pointers, or handle them differently if needed
+		}
+
+		// Append to string builder
+		if sb.Len() > 0 {
+			sb.WriteString("|")
+		}
+		sb.WriteString(fmt.Sprintf("%s:%v", tagName, value))
+	}
+
+	return sb.String()
 }
