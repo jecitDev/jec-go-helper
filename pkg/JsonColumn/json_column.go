120c2e84132d1bd6f45962147804463e1ee63880
fernandojec <fernando.riyo@jec.co.id>
2024-03-12 11:50:43 +0700

    v0.0.4

--- pkg/JsonColumn/json_column.go
+++ pkg/JsonColumn/json_column.go
@@ -6,23 +6,23 @@ import (
 )
 
 type JsonColumn[T any] struct {
-	v *T
+	V *T
 }
 
 func (j *JsonColumn[T]) Scan(src any) error {
 	if src == nil {
-		j.v = nil
+		j.V = nil
 		return nil
 	}
-	j.v = new(T)
-	return json.Unmarshal(src.([]byte), j.v)
+	j.V = new(T)
+	return json.Unmarshal(src.([]byte), j.V)
 }
 
 func (j *JsonColumn[T]) Value() (driver.Value, error) {
-	raw, err := json.Marshal(j.v)
+	raw, err := json.Marshal(j.V)
 	return raw, err
 }
 
 func (j *JsonColumn[T]) Get() *T {
-	return j.v
+	return j.V
 }
