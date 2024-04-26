89715a2e22c26a0ea8d0141ae6ff802d6fea8d84
fernandojec <fernando.riyo@jec.co.id>
2024-04-26 09:29:24 +0700

    v0.0.11

--- pkg/sliceTools/sliceTools.go
+++ pkg/sliceTools/sliceTools.go
@@ -2,20 +2,17 @@ package slicetools
 
 import "sort"
 
-func DeleteElement(slice interface{}, index int) interface{} {
-	sliceSlice := slice.([]interface{})
-	return append(sliceSlice[:index], sliceSlice[index+1:]...)
+func DeleteElement[T comparable](slice []T, index int) []T {
+	return append(slice[:index], slice[index+1:]...)
 }
 
-func DeleteElements(slice interface{}, indices []int) interface{} {
+func DeleteElements[T comparable](slice []T, indices []int) []T {
 	// Sort indices in descending order
 	sort.Sort(sort.Reverse(sort.IntSlice(indices)))
 
-	sliceSlice := slice.([]interface{})
-
 	for _, index := range indices {
-		sliceSlice = append(sliceSlice[:index], sliceSlice[index+1:]...)
+		slice = append(slice[:index], slice[index+1:]...)
 	}
 
-	return sliceSlice
+	return slice
 }
